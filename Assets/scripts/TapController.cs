12f5bfe59a4acd4a4426e5555df044adb51562f7
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-02-24 01:33:11 +0100

    completed tap controller and added to bird

--- Assets/scripts/TapController.cs
+++ Assets/scripts/TapController.cs
@@ -2,17 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class TapController : MonoBehaviour
 {
+
+    public float tapForce = 10.0f;
+    public float tiltSmooth = 5f;
+    public Vector3 startPosition;
+
+    Rigidbody2D rigidBody;
+    Quaternion downRotation;
+    Quaternion forwardRotation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
+        downRotation = Quaternion.Euler(0, 0, -90);
+        forwardRotation = Quaternion.Euler(0, 0, 35);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.rotation = forwardRotation;
+            rigidBody.velocity = Vector2.zero;
+            rigidBody.AddForce(Vector2.up * tapForce);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "ScoreZone")
+        {
+
+        }
+
+        if(collision.gameObject.tag == "DeadZone")
+        {
+            rigidBody.simulated = false;
+        }
     }
 }
