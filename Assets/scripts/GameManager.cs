d6e46375dd52d7cba74c8718e8e7bd432775c0a5
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-02-24 01:50:18 +0100

    Update GameManager.cs
    added fields that will be accessed

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -1,9 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
+    //game events that will be needed by other scripts
+    public delegate void GameDelegate();
+    public static event GameDelegate OnGameStarted;
+    public static event GameDelegate OnGameOverConfirmed;
+
+
+    //screens
+    public GameObject startScreen;
+    public GameObject countdownScreen;
+    public GameObject gameOverScreen;
+
+    //score text
+    public Text scoreText;
+
+    //create a singleton instance of gamemanager
+    public static GameManager Instance;
+
+    //page states
+    enum PageState
+    {
+        None,
+        Start,
+        GameOver,
+        CountDown
+    }
+
+    //game over state
+    bool gameOver = false;
+    int score = 0;
+
+    //return game over state
+    public bool GameOver { get { return gameOver; } }
+
+    //instantiate the singleton reference as game is started
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
